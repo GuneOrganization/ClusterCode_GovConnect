bcfc4d9f833c8d70c039e234652819ea0a4952a4
GuneOfficial <147017989+GuneOfficial@users.noreply.github.com>
2025-08-13 22:35:56 +0530

    file type changes and some miner changes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.php"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        teal: {
+          500: "#0d9488",
+          600: "#0f766e",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
