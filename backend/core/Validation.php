8cec8b60d77bfb9d946454f602a901dcefd75a1d
Sandeep Kavinda <sandeepkavinda203@gmail.com>
2025-08-15 17:40:25 +0530

    Create Validation.php

---
+++ backend/core/Validation.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'connection.php';
+
+class Validation
+{
+
+    public static function isValidUser($nic)
+    {
+        $resultSet = Database::search("SELECT * FROM user u
+            INNER JOIN user_status s ON u.user_status_id = s.id
+            WHERE nic='{$nic}'  AND s.status='Active'");
+        if ($resultSet != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function isValidUserWithRole($nic, $userRole)
+    {
+
+        $resultSet = Database::search("SELECT * FROM user u
+            INNER JOIN user_role r ON u.user_role_id = r.id
+            INNER JOIN user_status s ON u.user_status_id = s.id
+            WHERE nic='{$nic}' AND r.role='{$userRole}' AND s.status='Active'");
+
+        if ($resultSet != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
