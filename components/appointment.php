96938b349ef00240b42da11c52e46d5bd80d3a15
Sachintha Dilshan Chandradasa <150923784+dila2004@users.noreply.github.com>
2025-08-16 05:18:53 +0530

    update 2.html

--- components/appointment.php
+++ components/appointment.php
@@ -52,7 +52,7 @@
                         <div class="flex flex-col sm:flex-row gap-2 mt-4">
                             <button
                                 class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded text-sm font-medium transition-colors w-full sm:w-auto">CANCEL</button>
-                            <button id="openModalbarcode"
+                            <button id="openAppointmentModal"
                                 class="bg-teal-500 hover:bg-teal-600 text-white px-4 py-2 rounded text-sm font-medium transition-colors w-full sm:w-auto">VIEW
                                 BARCODE</button>
                             <button id="openFeedback"
@@ -87,5 +87,7 @@
 </div>
 
 <!--  -->
+
+
 
 
