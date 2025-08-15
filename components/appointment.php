6cd3bbf29d99d83491fe5b467f09c6e77e6040e1
Sachintha Dilshan Chandradasa <150923784+dila2004@users.noreply.github.com>
2025-08-16 05:20:25 +0530

    btun updatetd

--- components/appointment.php
+++ components/appointment.php
@@ -52,7 +52,7 @@
                         <div class="flex flex-col sm:flex-row gap-2 mt-4">
                             <button
                                 class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded text-sm font-medium transition-colors w-full sm:w-auto">CANCEL</button>
-                            <button id="openAppointmentModal"
+                            <button id="openModalbarcode"
                                 class="bg-teal-500 hover:bg-teal-600 text-white px-4 py-2 rounded text-sm font-medium transition-colors w-full sm:w-auto">VIEW
                                 BARCODE</button>
                             <button id="openFeedback"
